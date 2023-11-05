edb651062c728bd6c36a735dcb3ee6c0e17270c9
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2023-11-05 22:13:43 +0530

    ... updated search state and category state in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-'use client';
+
 import { useUser } from '@auth0/nextjs-auth0/client'
 import { Offside } from 'next/font/google'
 import { useContext } from 'react'
@@ -11,7 +11,7 @@ import { useRouter } from 'next/navigation'
 
 function Navbar() {
 
-    const { select, setSelect, sidebar_items, search_term, setSearch_term, setFeed } = useContext(FeedContext)
+    const { select, setSelect, sidebar_items} = useContext(FeedContext)
     const router = useRouter()
     const { user, error, isLoading } = useUser();
 
@@ -20,10 +20,9 @@ function Navbar() {
             searchInput: '',
         },
         onSubmit: values => {
-            router.push('/')
-            setSearch_term(() => values.searchInput)
+            router.push(`/search/query=${values.searchInput}`)
+            setTerm(() => values.searchInput)
             setSelect()
-            setFeed('search')
             //   alert(JSON.stringify(values, null, 2));
         },
     })
@@ -60,7 +59,7 @@ function Navbar() {
                                 />
 
                                 <button type="submit"
-                                    // onClick={setSearch_term(formik.values.searchInput)}
+
                                     className="py-3 px-3  inline-flex justify-center items-center gap-2 rounded-e-full border-0 font-semibold  hover:text-white hover:bg-red-500 hover:border-red-500 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
