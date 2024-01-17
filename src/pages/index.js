837fde70afac446ded5b52d8b003045035c0a1e1
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-01-17 12:14:00 +0530

    ...

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import Feed from '../components/Feed/Feed'
 import Navbar from '@/components/Navbar'
 
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
